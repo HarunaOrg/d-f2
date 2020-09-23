ca5648f7767ba808e23308de0286c9697d431dfc
nicholasstano <nicholasstano@gmail.com>
2020-09-23 13:09:06 -0400

    quick change

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.port || 3000
 const MONGODB_URI = 'mongodb+srv://nick:c0ldlampinwithFL4V4@cluster0.lwbmc.mongodb.net/movie-catalog?retryWrites=true&w=majority'
 
+// mongodb://heroku_2h1pzcsf:pfnq13jdlkerjt38lq2s4rq1r4@ds221228.mlab.com:21228/heroku_2h1pzcsf
+
 const movieRoutes = require('./routes/movieRoutes')
 
 app.use(bodyParser.json());
