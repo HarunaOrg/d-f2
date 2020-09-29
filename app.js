bf1963f2752aad0fdfb87ba18d963fbac9692ac8
nicholasstano <nicholasstano@gmail.com>
2020-09-29 16:59:03 -0400

    changed port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 const movieRoutes = require('./routes/movieRoutes')
 
